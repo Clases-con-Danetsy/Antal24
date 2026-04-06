2824ab932e8db65ce3268b6c41198bb7617e7ab9
CristianMeSu <cristian.mendoza.026@gmail.com>
2026-04-06 15:46:35 -0500

    multi idioma con variables de lenguaje

---
+++ src/locales.js
@@ -0,0 +1,19 @@
+import { map } from "astro:schema";
+
+export const translations = {
+  //region Español
+  es: {
+    header: {
+      title: "Español",
+    },
+  },
+  //endregion
+
+  //region Inglés
+  en: {
+    header: {
+      title: "Ingles",
+    },
+  },
+  //endregion
+};
