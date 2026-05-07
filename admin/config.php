fd7e4401252ebae5d5953c340884a8fb39ff8a08
CristianMeSu <cristian.mendoza.026@gmail.com>
2026-05-07 15:38:55 -0500

    se agrega deteccion automatica de entorno para config.php y se agrega a gitignore los archivos de .astro

--- admin/config.php
+++ admin/config.php
@@ -1,24 +1,27 @@
 <?php
 // ============================================
 // admin/config.php
-// Cambia estos valores con los de tu Hostinger
 // ============================================
 
-// Base de datos Produccion
-// define('DB_HOST', 'localhost');
-// define('DB_USER', 'u249540203_adminantal'); 
-// define('DB_PASS', 'Antal2026!!'); 
-// define('DB_NAME', 'u249540203_antal24'); 
-// define('SITE_URL', 'https://antal24.com');
+// Detectar entorno automáticamente
+$isLocal = in_array($_SERVER['SERVER_NAME'] ?? '', ['localhost', '127.0.0.1'])
+        || str_starts_with($_SERVER['SERVER_ADDR'] ?? '', '192.168.');
 
-// Base de datos local XAMPP
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'u249540203_antal24');
-define('SITE_URL', 'http://localhost/antal24');
+if ($isLocal) {
+    // Base de datos local XAMPP
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'u249540203_antal24');
+    define('SITE_URL', 'http://localhost/antal24');
+} else {
+    // Base de datos Producción (Hostinger)
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'u249540203_adminantal');
+    define('DB_PASS', 'Antal2026!!');
+    define('DB_NAME', 'u249540203_antal24');
+    define('SITE_URL', 'https://antal24.com');
+}
 
 // Contraseña para acceder al dashboard
-// Cámbiala por algo seguro
 define('ADMIN_PASSWORD', 'antal2026admin');
-
